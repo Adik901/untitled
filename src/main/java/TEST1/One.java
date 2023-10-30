6eead82e2c0c2bb117adecb7882eb28882cdfc09
adik <adikdima29@gmail.com>
2023-10-30 09:38:53 -0400

    I share

--- src/main/java/TEST1/One.java
+++ src/main/java/TEST1/One.java
@@ -2,9 +2,12 @@ package TEST1;
 
 public class One {
     public static void main(String[] args) {
+<<<<<<< HEAD
 
     }
     {
+=======
+>>>>>>> 0119abb (I share)
         System.out.println(" ПРИВЕТ MAIN");
     }
 }
--- src/test/java/TEST1/TEST_one.java
+++ src/test/java/TEST1/TEST_one.java
@@ -2,8 +2,12 @@ package TEST1;
 
 public class TEST_one {
     public static void main(String[] args) {
+<<<<<<< HEAD
 
     } {
         System.out.println("ПРивет TEST");
+=======
+        System.out.println(" ПРивет TEST");
+>>>>>>> 0119abb (I share)
     }
 }
